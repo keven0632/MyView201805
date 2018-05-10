0d75cd3ff506a32c127b85e0ec83187bb203be6f
zhengjian <keven0632@gmail.com>
2018-05-10 23:58:03 +0800

    2018-0510-11：57

--- app/src/main/java/com/kevenzheng/myview201805/view/MytextView01.java
+++ app/src/main/java/com/kevenzheng/myview201805/view/MytextView01.java
@@ -26,4 +26,32 @@ public class MytextView01 extends View {
     public MytextView01(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
+
+    /**
+     * 自定义View的测量
+     *
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //布局的宽高都是有这个方法指定
+        //指定控件的宽高，需要测量
+
+        //获取当前控件宽高的模式
+        int width_mode = MeasureSpec.getMode(widthMeasureSpec);
+        int heght_mode = MeasureSpec.getMode(heightMeasureSpec);
+
+
+        //MeasureSpec.EXACTLY 在布局中指定了确切的值 或者是match_parent
+        //MeasureSpec.AT_MOST 在布局中指定了wrap_content
+        //MeasureSpec.UNSPECIFIED 尽可能的大（不常用）ListView，ScroolView 在测量子布局的时候会使用UNSPECIFIED
+        if (width_mode == MeasureSpec.AT_MOST) {
+
+        } else {
+
+        }
+
+    }
 }
